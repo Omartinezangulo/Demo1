a8cfdb9380e382644a295604d3f27ccba179d02a
Omartinezangulo <103291599+Omartinezangulo@users.noreply.github.com>
2022-05-11 13:45:25 -0700

    push

--- demo.java
+++ demo.java
@@ -1,4 +1,6 @@
 public class demo{
-
+public static int sub(int a , int b){
+    return a-b;
+}
 
 }
---
+++ demoTestcase.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+
+public class demoTestcase {
+    @Test
+    public void subtract(){
+
+
+assertEquals(3, demo.sub(2, 1));
+    }
+    
+    
+}
